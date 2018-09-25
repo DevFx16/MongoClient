49b28ae5efa38a285f6b0bd4ad3a48063ff8dc10
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-09-25 09:56:56 -0500

    Dependencias

--- App.js
+++ App.js
@@ -2,6 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { Load: false }
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ Load: true });
+  }
+
   render() {
     return (
       <View style={styles.container}>
