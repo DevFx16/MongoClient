3f9f6b7d7fa44c66a07d781ccdb46aeb95a0cfdb
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-10-17 12:45:13 -0500

    Ng Model

--- src/app/Pages/agregar/agregar.page.ts
+++ src/app/Pages/agregar/agregar.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-agregar',
@@ -10,11 +11,36 @@ import { Router } from '@angular/router';
 export class AgregarPage implements OnInit {
 
   Opcion: boolean;
+  Conexion = { Host: '', Puerto: 80, Base: '', Auth: false, User: '', Password: '', Url: '' };
 
   constructor(private _Router: Router) { }
 
   ngOnInit() {
     this.Opcion = false;
+  }
+
+  ClickConexion(): void {
+    if(!this.Opcion){
+      if(this.Conexion.Url.length <= 0){
+        swal('Error', 'Url requerida', 'error');
+      }else{
+
+      }
+    }else{
+      if(this.Conexion.Host.length <= 0 || this.Conexion.Base.length <= 0 || this.Conexion.Puerto == null){
+        swal('Error', 'Todos los campos son requeridos', 'error');
+      }else{
+        if(this.Conexion.Auth){
+          if(this.Conexion.User.length <= 0 || this.Conexion.Password.length <= 0){
+            swal('Error', 'Todos los campos son requeridos', 'error');
+          }else{
+
+          }
+        }else{
+
+        }
+      }
+    }
   }
 
 }
