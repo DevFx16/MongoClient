a5278cefb47386bb630643ad2824a113b7c1b405
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-10-24 21:27:22 -0500

    Refresh y actionsheet

--- src/app/Pages/inicio/inicio.page.ts
+++ src/app/Pages/inicio/inicio.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { MongoConexionService } from '../../Services/MongoConexion/mongo-conexion.service';
+import { ActionSheetController } from '@ionic/angular';
 import Conexion from '../../Models/Conexion';
 import swal from 'sweetalert';
 
@@ -13,7 +14,7 @@ export class InicioPage implements OnInit {
 
   Conexiones: Conexion[];
 
-  constructor(private _Router: Router, private _Conexion: MongoConexionService) {
+  constructor(private _Router: Router, private _Conexion: MongoConexionService, private _Action: ActionSheetController) {
     this._Router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
         if (this._Router.url == '/Inicio') {
@@ -31,17 +32,56 @@ export class InicioPage implements OnInit {
     this._Router.navigate(['/Agregar']);
   }
 
+  Refrescar(event) {
+    setTimeout(() => {
+      this.Conexiones = this._Conexion.RetornarConexiones();
+      event.target.complete();
+    }, 1500);
+  }
+
+  async Accion(index: number) {
+    const actionSheet = await this._Action.create({
+      header: 'Opciones',
+      buttons: [{
+        text: 'Borrar',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.EliminarConexion(index);
+        }
+      }, {
+        text: 'Ver',
+        icon: 'ios-eye',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Clancelar',
+        icon: 'close',
+        role: 'cancel',
+      }],
+      backdropDismiss: false,
+      keyboardClose: true,
+      mode: 'ios'
+    });
+    await actionSheet.present();
+  }
+
   EliminarConexion(index: number): void {
-    swal({
-      title: '¿Esta seguro de querer eliminar esta conexión?',
-      icon: 'warning',
-      buttons: ['Cancelar', 'Eliminar'],
-      dangerMode: true
-    }).then(value => {
-      if (value) {
-        this.Conexiones = this._Conexion.EliminarConexion(index);
-      }
-    })
+    if (this.Conexiones.length <= 0) {
+      swal('Agregado', 'No hay mas conexiónes', 'error');
+    } else {
+      swal({
+        title: '¿Esta seguro de querer eliminar esta conexión?',
+        icon: 'warning',
+        buttons: ['Cancelar', 'Eliminar'],
+        dangerMode: true
+      }).then(value => {
+        if (value) {
+          this._Conexion.EliminarConexion(index);
+        }
+      })
+    }
   }
 
 }
